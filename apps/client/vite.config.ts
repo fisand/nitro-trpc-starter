55715f94baf7604c2bf8bfb544fe0831b30201cc
zouhang <zouhangsweet@gmail.com>
2024-08-05 17:58:22 +0800

    chore: update logic

--- apps/client/vite.config.ts
+++ apps/client/vite.config.ts
@@ -54,7 +54,6 @@ export default defineConfig({
       '/api': {
         target: 'http://localhost:3000',
         changeOrigin: true,
-        rewrite: (path) => path.replace(/^\/api/, ''),
       },
     },
   },
