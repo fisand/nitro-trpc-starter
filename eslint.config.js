b96aa49f03da2926c67056bcbb91d324ccd98c05
zouhang <zouhangsweet@gmail.com>
2024-08-07 18:14:54 +0800

    chore: update logic

--- eslint.config.js
+++ eslint.config.js
@@ -1,9 +1,11 @@
 import unocss from '@unocss/eslint-config/flat'
 import { defineConfig } from 'eslint-config-hyoban'
 
-export default defineConfig({}, [
-  {
-    files: ['**/*.{ts,tsx}'],
-    ...unocss,
-  },
-])
+export default defineConfig({
+  react: 'vite',
+  restrictedSyntax: ['jsx', 'tsx'],
+  strict: true,
+}, {
+  files: ['**/*.{ts,tsx}'],
+  ...unocss,
+})
