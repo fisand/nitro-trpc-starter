159001d5a3b6709d0992b2197b2035499e69640e
zouhang <zouhangsweet@gmail.com>
2024-08-07 16:10:48 +0800

    chore: update motion

--- apps/client/src/pages/index.tsx
+++ apps/client/src/pages/index.tsx
@@ -1,3 +1,4 @@
+import { motion } from 'framer-motion'
 import { toast } from 'sonner'
 
 import { trpc } from '../trpc'
@@ -10,7 +11,11 @@ const HomePage = () => {
 
   return (
     <div className="h-screen w-screen gap-5">
-      <div className="h-14 border-b border-b-1 border-border">
+      <motion.div
+        initial={{ opacity: 0, y: -6 }}
+        animate={{ opacity: 1, y: 0 }}
+        className="h-14 border-b border-b-1 border-border"
+      >
         <div className="mx-auto h-full flex items-center justify-between container lt-sm:px-4">
           <span className="flex items-center gap-1.5 text-lg font-500">
             <span className="i-devicon-plain:trpc h-5.5 w-5.5"></span>
@@ -26,47 +31,72 @@ const HomePage = () => {
             <span className="i-simple-icons:github h-5.5 w-5.5 cursor-pointer transition-all hover:scale-105"></span>
           </a>
         </div>
-      </div>
+      </motion.div>
       <div className="mx-auto w-100 lt-sm:px-4">
         <div className="flex items-center gap-2 pb-5 pt-10">
-          {userList.data?.map((user) => <Button key={user.id}>{user.name}</Button>)}
+          {userList.data?.map((user, index) => (
+            <motion.span
+              key={user.id}
+              custom={index}
+              initial="hidden"
+              animate="visible"
+              variants={{
+                visible: (i: number) => ({
+                  opacity: 1,
+                  y: 0,
+                  transition: {
+                    delay: (i + 0.05) * 0.05,
+                  },
+                }),
+                hidden: { opacity: 0, y: 10 },
+              }}
+            >
+              <Button>{user.name}</Button>
+            </motion.span>
+          ))}
         </div>
-        <div className="flex items-center gap-2">
-          <Input
-            type="text"
-            maxLength={5}
-            value={name}
-            onInput={(e) => {
-              setName((e.target as HTMLInputElement).value)
-            }}
-            placeholder="Name"
-            className="w-40"
-          />
-          <Button
-            onClick={async () => {
-              if (!name) {
-                toast.error('Name is required')
-                return
-              }
-              if (userList.data && userList.data?.length >= 6) {
-                toast.error('You have reached the maximum number of users')
-                return
-              }
-
-              await createUser.mutateAsync({ name })
-              setName('')
-              await userList.refetch()
-            }}
-            disabled={userList.isPending || !userList.data || createUser.isPending}
+        {!userList.isPending && (
+          <motion.div
+            initial={{ opacity: 0, y: 10 }}
+            animate={{ opacity: 1, y: 0 }}
+            className="flex items-center gap-2"
           >
-            {createUser.isPending ? (
-              <span className="i-lucide:loader-circle mr-1 h-4 w-4 animate-spin" />
-            ) : (
-              <span className="i-lucide:plus mr-1 h-4 w-4 text-primary-foreground"></span>
-            )}
-            New
-          </Button>
-        </div>
+            <Input
+              type="text"
+              maxLength={5}
+              value={name}
+              onInput={(e) => {
+                setName((e.target as HTMLInputElement).value)
+              }}
+              placeholder="Name"
+              className="w-40"
+            />
+            <Button
+              onClick={async () => {
+                if (!name) {
+                  toast.error('Name is required')
+                  return
+                }
+                if (userList.data && userList.data?.length >= 6) {
+                  toast.error('You have reached the maximum number of users')
+                  return
+                }
+
+                await createUser.mutateAsync({ name })
+                setName('')
+                await userList.refetch()
+              }}
+              disabled={userList.isPending || !userList.data || createUser.isPending}
+            >
+              {createUser.isPending ? (
+                <span className="i-lucide:loader-circle mr-1 h-4 w-4 animate-spin" />
+              ) : (
+                <span className="i-lucide:plus mr-1 h-4 w-4 text-primary-foreground"></span>
+              )}
+              New
+            </Button>
+          </motion.div>
+        )}
       </div>
     </div>
   )
